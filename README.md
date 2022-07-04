7cac4ca50c042d3d806b672ca31f18322203a3f6
Matheus Maia Pimenta Delgado <>
2022-07-04 15:32:41 -0300

    Initial commit. Added all sample code.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup after download
+
+Just run the following code before trying to reproduce the issue:
+
+```bash
+git submodule init
+git submodule update --recursive
+```
---
+++ snippet.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from typing import TypeAlias, Union
+from functools import total_ordering
+
+
+class Interface:
+    pass
+
+
+InterfaceOrInt: TypeAlias = Union[Interface, int]
+
+
+@total_ordering
+class FooDict(Interface, dict[int, InterfaceOrInt]):
+
+    def foo(self) -> None:
+        bar: InterfaceOrInt = self[0]
+        if isinstance(bar, (FooDict, FooList)):
+            _ = bar.sorted()
+
+    def sorted(self) -> "FooDict":
+        return FooDict()
+
+    def __lt__(self, __o: object) -> bool:
+        return True
+
+
+@total_ordering
+class FooList(Interface, list[InterfaceOrInt]):
+
+    def sorted(self) -> "FooList":
+        return FooList()
